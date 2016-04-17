57721ec2864a23a0e4178c6a95d2f77611fe8eb5
Aditya Malusare <150260005@iitb.ac.in>
2016-04-17 14:32:12 +0530

    Made the basic tweet capture mechanism

--- web/twitter.py
+++ web/twitter.py
@@ -1,5 +1,5 @@
 #just a BTW, the APi on twitter allows searching statements with a definite positive/negative. Look into it
-#create table mem.twt(query, tweet, time)
+#create table mem.twt(query varchar(50), tweet varchar(150), time varchar(20)
 
 from tweepy import Stream
 from tweepy import OAuthHandler
@@ -8,22 +8,29 @@ import MySQLdb
 import time
 import json
 
-
+ckey='pTlSX6TKUaoHDVF67BgF3yHDY'
+csecret='OVQelHvTtHuHlpmQa8IoSYKEABTAHHHYMtPN2DeqOVnJsdKLhD'
+atoken='720586054223339521-Uo4T10dtKwUK7yp7OQFR51k0X9fxfeT'
+asecret='wIWzrm3DmRw4yr6hb06KbmactF8HxC7fikpzhew5d9u26'
 
 class listener(StreamListener):
 
-    def on_data(self, data):
+    def on_data(self, qu, data):
         all_data = json.loads(data)        
         tweet = all_data["text"]
         username = all_data["user"]["screen_name"]
+        ts = all_data["timestamp_ms"]
         
-        c.execute("INSERT INTO taula (time, username, tweet) VALUES (%s,%s,%s)",
-            (time.time(), username, tweet))
+        c.execute("insert into twt VALUES('"+qu+"','"+tweet+"','"+ts+"')")
+
+        #print all_data
+        #print '-------------------------------------------------------------'
+
+        #c.execute("INSERT INTO taula (time, username, tweet) VALUES (%s,%s,%s)",
+        #            (time.time(), username, tweet))
 
         conn.commit()
 
-        print((username,tweet))
-        
         return True
 
     def on_error(self, status):
@@ -31,7 +38,7 @@ class listener(StreamListener):
 
 def track(filtername):
 
-	conn = MySQLdb.connect(user="root",passwd="aditya",db="mem")
+	conn = MySQLdb.connect(user="root",passwd="aditya",db="test")
 	c = conn.cursor()
 
 	auth = OAuthHandler(ckey, csecret)
