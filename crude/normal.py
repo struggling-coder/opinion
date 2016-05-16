b71952675968954014490f647121fb726b5e960c
Aditya Malusare <aditya.malusare@gmail.com>
2016-05-16 23:37:19 +0530

    Completed the normal word scanning system.
    What's left is the most important part

--- crude/normal.py
+++ crude/normal.py
@@ -3,15 +3,21 @@ import dbctrl, MySQLdb
 
 datapath = ''
 
+#create table normal_proc1(word varchar(20), freq int)
+#create table normal_proc2(word varchar(20), times mediumint)
+
+def assimilate_changes():
+	'''Backup first. Make changes to cmem REMEMBER CMEM'''
+
 def process_data(table_name):
 	'''Think about it'''
 
-#create table normal_proc1(word varchar(20))
-#create table normal_proc2(word varchar(20), times mediumint)
-def acquire_data(data, db=None):
+def acquire_data(data, db=None, verbose=False):
 	'''Use the data standard. This will require more work.'''
 
-	if (db=None):
+	if verbose: return acquire_data_verbose(data, db)
+
+	if (db is None):
 		db = dbctrl.handledb('adj')
 
 	conn = MySQLdb.connect(user='root', passwd='aditya', db='mem')	
@@ -23,11 +29,42 @@ def acquire_data(data, db=None):
 	for e in data:
 		whoosh = text.tokenize(e)
 		for word in whoosh:
-			c.execute("insert into normal_proc1 values('"+word+"')")
+			c.execute("insert into normal_proc1 values('"+word+"', 1)")
 		conn.commit()
 
 	c.execute("insert into normal_proc2 select word, sum(freq) from normal_proc1 group by word")
 	conn.commit()
 	conn.close()
 
+def acquire_data_verbose(data, db=None):
+	
+	if (db is None):
+		db = dbctrl.handledb('adj')
+
+	conn = MySQLdb.connect(user='root', passwd='aditya', db='mem')	
+	c= conn.cursor()
+
+	print "connection openend to mem"
+
+	c.execute("delete from normal_proc1")
+	c.execute("delete from normal_proc2")
+
+	print 'data wiped'
+
+	for e in data:
+		whoosh = text.tokenize(e)
+		print 'text tokenized'
+		for word in whoosh:
+			c.execute("insert into normal_proc1 values('"+word+"', 1)")
+		conn.commit()
+
+	print 'table 1 done'
+
+	c.execute("insert into normal_proc2 select word, sum(freq) from normal_proc1 group by word")
+	conn.commit()
+
+	print 'table 2 done'
+	conn.close()
+
 def get_data_linux():
+	'''Whatever'''
